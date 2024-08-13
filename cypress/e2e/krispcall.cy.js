ca308c6de4fd87ac1c8e95b11018be0514bf97a1
Subhekshya Baral <baralshubu@gmail.com>
2024-08-13 12:50:38 +0545

    add id user name

--- cypress/e2e/krispcall.cy.js
+++ cypress/e2e/krispcall.cy.js
@@ -3,9 +3,9 @@ describe('template spec', () => {
     cy.visit('https://www.saucedemo.com/');
     cy.url().should('eq', 'https://www.saucedemo.com/');
     cy.contains('Swag Labs')
-    
-    cy.get('input[name="Username"]').type(standard_user);
-    cy.get('input[name="Password"]').type(secret_sauce);
-    cy.get('button[type="Login"]').click();
+
+    cy.get('#user-name').type(standard_user);
+    cy.get('#password').type(secret_sauce);
+    cy.get('#login-button').click();
   })
 })
