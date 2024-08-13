e4bb7317214a94420c897d32eb8dee595765391f
Subhekshya Baral <baralshubu@gmail.com>
2024-08-13 10:45:41 +0545

    Assert that the url is correct

--- cypress/e2e/krispcall.cy.js
+++ cypress/e2e/krispcall.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.saucedemo.com/')
-    
+    cy.visit('https://www.saucedemo.com/');
+    cy.url().should('eq', 'https://www.saucedemo.com/');
   })
 })
