bca8893aee89c23c0a78080aa44d4a0819da8f02
Subhekshya Baral <baralshubu@gmail.com>
2024-08-13 13:03:28 +0545

    add credential

--- cypress/e2e/krispcall.cy.js
+++ cypress/e2e/krispcall.cy.js
@@ -4,8 +4,8 @@ describe('template spec', () => {
     cy.url().should('eq', 'https://www.saucedemo.com/');
     cy.contains('Swag Labs')
 
-    cy.get('#user-name').type(standard_user);
-    cy.get('#password').type(secret_sauce);
+    cy.get('#user-name').type("standard_user");
+    cy.get('#password').type("secret_sauce");
     cy.get('#login-button').click();
   })
 })
