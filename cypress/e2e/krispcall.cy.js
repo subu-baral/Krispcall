e6cbe3fc7e10dc56216641d0e9b1b95e6be380b3
subhekshya Baral <baralshubu@gmail.com>
2024-08-15 09:36:08 +0545

    add credential

--- cypress/e2e/krispcall.cy.js
+++ cypress/e2e/krispcall.cy.js
@@ -1,15 +1,15 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.url().should('eq', 'https://www.saucedemo.com/');
-    cy.contains('Swag Labs')
+   // cy.visit('https://www.saucedemo.com/');
+    //cy.url().should('eq', 'https://www.saucedemo.com/');
+    //cy.contains('Swag Labs')
 
-    cy.get('#user-name').type("standard_user");
-    cy.get('#password').type("secret_sauce");
-    cy.get('#login-button').click();
+    //cy.get('#user-name').type("standard_user");
+    //cy.get('#password').type("secret_sauce");
+    //cy.get('#login-button').click();
 
-    cy.get(".bm-burger-button").click()
-    cy.contains("About")
-    cy.get(".secret_sauce").click()
+    //cy.get(".bm-burger-button").click()
+    //cy.contains("About")
+    //cy.get(".secret_sauce").click()
   })
 })
--- cypress/e2e/swag_lab/login.cy.js
+++ cypress/e2e/swag_lab/login.cy.js
@@ -1,5 +1,17 @@
 describe ('Login page', ()=>{
     it ('Valid login with standard user', ()=>{
-        
+        // visit the swag lab page
+        cy.visit('https://www.saucedemo.com/')
+        cy.contains('Swag Labs')
+        // placeholders should be visible and clickable
+        cy.get('input[name="user-name"]').should('have.attr', 'placeholder', 'Username').click()
+        cy.get('input[name="password"]').should('have.attr', 'placeholder', 'Password').click()
+         cy.get('input[name="login-button"]').should('have.value', 'Login').click()
+      //  Cypress.Commands.add('clickUsernameField', (selector) => {
+        //    cy.get(selector).should('be.visible').and('have.attr', 'placeholder').click();
+       // })
+        //Cypress.Commands.add('clickPasswordField', (selector) => {
+         //   cy.get(selector).should('be.visible').and('have.attr', 'placeholder').click();
+       // })
     })
 })
